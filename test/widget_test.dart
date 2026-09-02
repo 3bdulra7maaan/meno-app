327f2508a159d4b14a5f2d1f03f4f143f7d34c6c
3bdulra7maaan <146699822+3bdulra7maaan@users.noreply.github.com>
2026-09-02 12:01:32 +0300

    fix: import Material theme in font test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/services.dart';
 import 'package:meno/data/in_memory_question_repository.dart';
